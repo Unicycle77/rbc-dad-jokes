431031c97735e4c883362576deb80b0fddddf2cd
Jeffrey Martin <unicycle77@gmail.com>
2021-08-02 11:03:38 -0700

    Style jokes

--- src/Joke.js
+++ src/Joke.js
@@ -7,7 +7,7 @@ class Joke extends Component {
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.props.upvote} />
-          <span>{this.props.votes}</span>
+          <span className="Joke-votes">{this.props.votes}</span>
           <i className="fas fa-arrow-down" onClick={this.props.downvote} />
         </div>
         <div className="Joke-text">{this.props.text}</div>
